26ed339941e7a38ca61e74d38ce2387a70aac900
arikaim <info@arikaim.com>
2022-01-23 13:48:50 -0500

    =

---
+++ README.md
@@ -0,0 +1,17 @@
+## Arikaim CMS Web3 Module
+![1.0.0](https://img.shields.io/github/release/arikaim/web3-module.svg)
+![GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+Web3 module
+
+#### Requirements  
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+
+
+#### Installation
+
+```sh
+
+composer require arikaim/web3-module
+
+```
---
+++ Web3.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c) Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Web3;
+
+use Arikaim\Core\Extension\Module;
+
+/**
+ * Web3 module class
+ */
+class Web3 extends Module
+{
+    /**
+     * Install module
+     *
+     * @return void
+     */
+    public function install()
+    {        
+    }
+}
